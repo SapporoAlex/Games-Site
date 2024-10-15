bba8e76dc13f09391faa3ab4b63a81cab394be03
Alex_McKinley <alexanderfromaustralia@gmail.com>
2024-10-15 16:13:01 +0900

    hammy racing fix

--- game/static/Hammy-Racing-2024/game.js
+++ game/static/Hammy-Racing-2024/game.js
@@ -19,27 +19,6 @@ obstacleImage2.src = '/static/Hammy-Racing-2024/coins.png'; // Replace with the 
 const finishLineImage = new Image();
 finishLineImage.src = '/static/Hammy-Racing-2024/finish.png'; // Replace with the path to your second obstacle image
 
-document.getElementById('upButton').addEventListener('touchstart', () => {
-    car.dy = -car.speed;
-});
-
-document.getElementById('downButton').addEventListener('touchstart', () => {
-    car.dy = car.speed;
-});
-
-document.getElementById('upButton').addEventListener('touchend', () => {
-    car.dy = 0;
-});
-
-document.getElementById('downButton').addEventListener('touchend', () => {
-    car.dy = 0;
-});
-
-document.getElementById('restartButton').addEventListener('click', () => {
-    if (gameOver) {
-        restartGame();
-    }
-});
 
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
